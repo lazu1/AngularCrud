2eb88706af4e2749a7c2ff95da86f166bd0e93dd
lazana <desarlazana11@gmail.com>
2019-12-08 18:00:24 +0545

    product add html and ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { ProductAddComponent } from './product-add/product-add.component';
 import { ProductGetComponent } from './product-get/product-get.component';
 import { ProductEditComponent } from './product-edit/product-edit.component';
+import { ReactiveFormsModule } from '@angular/forms'
 
 @NgModule({
   declarations: [
@@ -17,7 +18,9 @@ import { ProductEditComponent } from './product-edit/product-edit.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    SlimLoadingBarModule
+    SlimLoadingBarModule,
+    ReactiveFormsModule
+
 
   ],
   providers: [],
